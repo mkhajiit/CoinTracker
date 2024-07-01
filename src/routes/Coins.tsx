6d11dde5702ec7948ba9dd0c366c0ac8fc845253
mkhajiit <mkhajiit15@gmail.com>
2024-07-01 22:37:54 +0900

    axios로 코인 데이터 받아오기 구현

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Title, Container, Header, CoinList, Coin } from './Coins.styles';
-import coins from '../data/coinData';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 export default function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]); // 배열의 타입을 지정할때 [] 반드시 붙여야함
+  const [loading, setLoading] = useState(true);
+
+  //axios 사용
+  const getCoins = async () => {
+    const res = await axios('https://api.coinpaprika.com/v1/coins');
+    console.log(res);
+    setCoins(res.data.slice(0, 100));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    // (function)()즉시실행함수 fetch 사용
+
+    //   (
+    //     async () => {
+    //     const response = await fetch('https://api.coinpaprika.com/v1/coins');
+    //     const json = await response.json();
+    //     console.log(json);
+    //     setCoins(json.slice(0, 100));
+    //     setLoading(false);
+    //   }
+    // )();
+    getCoins();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinList>
+      {loading ? (
+        'Loading ...'
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
     </Container>
   );
 }
